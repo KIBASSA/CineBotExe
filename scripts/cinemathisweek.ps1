043aace837d1e6eceb453a3768f8fb554dd9fb1a
junior kibassa <juniorkibassa2007@gmail.com>
2016-07-03 16:49:59 +0200

    good

--- scripts/cinemathisweek.ps1
+++ scripts/cinemathisweek.ps1
@@ -1,6 +1,12 @@
 write-host "##teamcity[message text='junior']"
-#Start-Process -FilePath "CineParser.exe" -ArgumentList "test"
 
-start-process -filepath 'cineparser.exe'
+#$Username = "Administrator"
+#$Password = "TECKIONS2016"
+
+#$Credential = New-Object System.Management.Automation.PSCredential ("$UserName",(ConvertTo-SecureString -String $Password -AsPlainText -Force))
+
+#start-process -filepath 'cineparser.exe' -ArgumentList "-mode cinemaalwaysshowing" -Credential $Credential
+
+start-process -filepath 'cineparser.exe' -ArgumentList "-mode cinemaalwaysshowing"
 
 write-host "##teamcity[message text='kibassa']"
