0c2fc0e7bc440a6a75fa74d820cd0c1b249603e4
junior kibassa <juniorkibassa2007@gmail.com>
2016-08-25 00:41:05 +0200

    modify conversation text

--- scripts/conversation-category.ps1
+++
@@ -1,5 +0,0 @@
-write-host "##teamcity[message text='junior']"
-
-start-process -filepath 'BatchConversation.exe' -ArgumentList "-mode category-investigations"
-
-write-host "##teamcity[message text='kibassa']"
