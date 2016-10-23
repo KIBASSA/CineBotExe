9d541dcd4757f89bff9ddd4d2f8ad86c18e26dee
junior kibassa <juniorkibassa2007@gmail.com>
2016-10-23 23:15:36 +0200

    add files

---
+++ scripts/movieprovidedByCineTrafic.ps1
@@ -0,0 +1,12 @@
+write-host "##teamcity[message text='junior']"
+
+#$Username = "Administrator"
+#$Password = "TECKIONS2016"
+
+#$Credential = New-Object System.Management.Automation.PSCredential ("$UserName",(ConvertTo-SecureString -String $Password -AsPlainText -Force))
+
+#start-process -filepath 'cineparser.exe' -ArgumentList "-mode cinemaalwaysshowing" -Credential $Credential
+
+start-process -filepath 'BatchConversation.exe' -ArgumentList "-mode provider-by-cinetrafic"
+
+write-host "##teamcity[message text='kibassa']"
