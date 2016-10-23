88e65d72844a861b806ccb20a2d17f91f31f232d
junior kibassa <juniorkibassa2007@gmail.com>
2016-10-23 23:24:39 +0200

    update scripts

--- scripts/movieprovidedBySensCritics.ps1
+++ scripts/movieprovidedBySensCritics.ps1
@@ -1,12 +1,5 @@
 write-host "##teamcity[message text='junior']"
 
-#$Username = "Administrator"
-#$Password = "TECKIONS2016"
-
-#$Credential = New-Object System.Management.Automation.PSCredential ("$UserName",(ConvertTo-SecureString -String $Password -AsPlainText -Force))
-
-#start-process -filepath 'cineparser.exe' -ArgumentList "-mode cinemaalwaysshowing" -Credential $Credential
-
-start-process -filepath 'BatchConversation.exe' -ArgumentList "-mode provider-by-senscritique"
+start-process -filepath 'CineParser.exe' -ArgumentList "-mode provider-by-senscritique"
 
 write-host "##teamcity[message text='kibassa']"
