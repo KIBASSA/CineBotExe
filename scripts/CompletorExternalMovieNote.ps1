91744d9dab36b9357f9a4c633a28e414543f83d2
junior kibassa <juniorkibassa2007@gmail.com>
2017-04-01 21:43:10 +0200

    add script for external movie

---
+++ scripts/CompletorExternalMovieNote.ps1
@@ -0,0 +1,12 @@
+write-host "##teamcity[message text='junior']"
+
+#$Username = "Administrator"
+#$Password = "TECKIONS2016"
+
+#$Credential = New-Object System.Management.Automation.PSCredential ("$UserName",(ConvertTo-SecureString -String $Password -AsPlainText -Force))
+
+#start-process -filepath 'cineparser.exe' -ArgumentList "-mode cinemaalwaysshowing" -Credential $Credential
+
+start-process -filepath 'BatchCompletor.exe' -ArgumentList "-mode complete-note-movie-on-display -start 0 -offset 400"
+
+write-host "##teamcity[message text='kibassa']"
