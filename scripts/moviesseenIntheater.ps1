16296382ebf4f249b8093bc38087cd6ac015dcf2
junior kibassa <juniorkibassa2007@gmail.com>
2017-04-16 22:41:40 +0200

    push

---
+++ scripts/moviesseenIntheater.ps1
@@ -0,0 +1,12 @@
+write-host "##teamcity[message text='junior']"
+
+#$Username = "Administrator"
+#$Password = "TECKIONS2016"
+
+#$Credential = New-Object System.Management.Automation.PSCredential ("$UserName",(ConvertTo-SecureString -String $Password -AsPlainText -Force))
+
+#start-process -filepath 'cineparser.exe' -ArgumentList "-mode cinemaalwaysshowing" -Credential $Credential
+
+start-process -filepath 'BatchConversation.exe' -ArgumentList "-mode movies-to-see-in-theater"
+
+write-host "##teamcity[message text='kibassa']"
