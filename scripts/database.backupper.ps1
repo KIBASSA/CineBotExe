82500727ad7edc9de159edfc6bd908357073da75
junior kibassa <juniorkibassa2007@gmail.com>
2017-07-09 13:26:37 +0200

    update backupper script

--- scripts/database.backupper.ps1
+++ scripts/database.backupper.ps1
@@ -1,11 +1,15 @@
 ﻿Set-Location "D:\CineBotData\DatabaseBackUp";
 
-$importExe = (Get-Location).Path + "\mongoexport.exe";
+$dumpExe = (Get-Location).Path + "\mongodump.exe";
 
-& $importExe --host '51.255.207.126:27017' --db cinebot --collection Cinema -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'cinema.json';
-& $importExe --host '51.255.207.126:27017' --db cinebot --collection Person -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'person.json';
-& $importExe --host '51.255.207.126:27017' --db cinebot --collection UserConversations -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'userconversations.json';
-& $importExe --host '51.255.207.126:27017' --db cinebot --collection News -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'news.json'
-& $importExe --host '51.255.207.126:27017' --db cinebot --collection ContributionMoviesProvider -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'contributionmoviesprovider.json';
+& $dumpExe --host '51.255.207.126:27017' -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' -d cinebot
+
+#$importExe = (Get-Location).Path + "\mongoexport.exe";
+
+#& $importExe --host '51.255.207.126:27017' --db cinebot --collection Cinema -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'cinema.json';
+#& $importExe --host '51.255.207.126:27017' --db cinebot --collection Person -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'person.json';
+#& $importExe --host '51.255.207.126:27017' --db cinebot --collection UserConversations -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'userconversations.json';
+#& $importExe --host '51.255.207.126:27017' --db cinebot --collection News -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'news.json'
+#& $importExe --host '51.255.207.126:27017' --db cinebot --collection ContributionMoviesProvider -u 'adminemiko' -p 'KIBASSA13MALIBA' --authenticationDatabase 'admin' --out 'contributionmoviesprovider.json';
 
 
